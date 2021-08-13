791deb7f54a58ec26506317e72282c56cca5ddf5
Yaoxian <yaoxian@gmail.com>
2021-08-13 20:18:17 +0800

    Update notifications.js

--- jquery/notifications.js
+++ jquery/notifications.js
@@ -1,7 +1,7 @@
 // Add your notification into "notifications" and separate them with a comma. Ensure that they are in double quotes.
 
 const custom_notifications = [
-  "<b>[Attention: System Maintenance]</b> GoBusiness Licensing will be unavailable due to scheduled maintenance from 13 Aug 2021 8pm to 14 Aug 2021 8am. Guided Journey for Food Services will remain available. We apologise for the inconvenience caused.",
+  "<b>[Attention: System Maintenance]</b> GoBusiness Licensing will be unavailable due to scheduled maintenance from 13 Aug 2021 8pm to 14 Aug 2021 8am.",
   "<b>[Announcement]</b> We have updated our Helpdesk contact information. Please go to our Contact Us page for the new contact details. Thank you.",
 ];
 
