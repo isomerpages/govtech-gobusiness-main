1cbde0cda42bbd97b482e0219388af3b06d8d93d
Yaoxian <yaoxian@gmail.com>
2021-08-27 13:20:01 +0800

    Update resize-tables.js

--- jquery/resize-tables.js
+++ jquery/resize-tables.js
@@ -1,7 +1,7 @@
 const customTables = [
   {
     path: "/contact-us/",
-    columns: ["32%", "auto",],
+    columns: ["22%", "auto",],
   },
   {
     path: "/covid-19-faqs/workplace-safe-management-measures",
