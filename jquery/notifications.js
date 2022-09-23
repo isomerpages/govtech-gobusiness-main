346b6134d359fc51f4225443fedb76e6b55a1236
Yaoxian <yaoxian.gtwork@gmail.com>
2022-09-23 13:48:57 +0800

    Update notifications.js

--- jquery/notifications.js
+++ jquery/notifications.js
@@ -11,7 +11,7 @@ const custom_notifications = [
   //[**LICENCEONE DOWNTIME**]
   //"<b>[Attention: System Maintenance]</b> GoBusiness Licensing will be unavailable due to a scheduled maintenance from 19 Aug 2022 10pm to 20 Aug 2022 6am. Guided Journey for Food Services will remain available. We apologise for the inconvenience caused.",
   //"<b>[Attention: System Maintenance]</b> GoBusiness Licensing will be unavailable due to an urgent maintenance from 2 Sep 2022 2pm to 4pm. Guided Journey for Food Services will remain available. We apologise for the inconvenience caused."
-  "<b>[Attention: System Maintenance]</b> GoBusiness Licensing will be unavailable on the dates below due to scheduled maintenance. Guided Journey for Food Services will remain available. We apologise for the inconvenience caused.<br>(1) From 23 Sep 2022 10pm to 24 Sep 2022 6am<br>(2) From 24 Sep 2022 10pm to 25 Sep 2022 6am<br>(3) From 25 Sep 2022 10pm to 26 Sep 2022 6am"
+  "<b>[Attention: System Maintenance]</b> GoBusiness Licensing will be unavailable on the dates below due to scheduled maintenance. Guided Journey for Food Services will remain available. We apologise for the inconvenience caused.<br>(1) From 23 Sep 2022 10pm to 24 Sep 2022 6am<br>(2) From 24 Sep 2022 10pm to 25 Sep 2022 11am"
 
   //[**ACRA DOWNTIME**]
   //"<b>[Attention: System Maintenance]</b> The following services will be unavailable due to scheduled maintenance from 20 Aug 2022 12am to 12pm: Name reservation application and business registration for Sole Proprietorship/Partnership, and information from ACRA for GoBusiness and Business Grants. We apologise for the inconvenience caused."
