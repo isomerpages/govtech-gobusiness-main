1c46e16d202b631fd33c3bc50446d785efcc2414
Yaoxian <yaoxian.gtwork@gmail.com>
2023-01-15 19:38:17 +0800

    fix table

--- jquery/resize-tables.js
+++ jquery/resize-tables.js
@@ -1,51 +1,51 @@
 const customTables = [
   {
     path: "/enterprisejobskills/resource-and-toolkits/skillfully",
-    columns: ["250px", "auto"],
+    columns: ["250px", "auto"]
   },
   {
     path: "/start-a-business/open-a-corporate-bank-account/",
-    columns: ["32%", "auto"],
+    columns: ["32%", "auto"]
   },
   {
     path: "/contact-us/",
-    columns: ["32%", "auto"],
+    columns: ["32%", "auto"]
   }, 
   {
     path: "/timelimitedrrt/",
-    columns: ["32%", "auto"],
+    columns: ["32%", "auto"]
   }, 
   {
     path: "/covid-19-faqs/workplace-safe-management-measures",
-    columns: ["32%", "auto"],
+    columns: ["32%", "auto"]
   },
   {
     path: "/licensing-faqs/food-shop-licence/",
-    columns: ["12%", "70%", "auto"],
+    columns: ["12%", "70%", "auto"]
   },
   {
     path: "/licensing-faqs/food-stall-licence/",
-    columns: ["12%", "70%", "auto"],
+    columns: ["12%", "70%", "auto"]
   },
   {
     path: "/productivity-solutions-grant/",
-    columns: ["20%", "auto"],
+    columns: ["20%", "auto"]
   },
   {
     path: "/browse-all-solutions*",
-    columns: ["auto", "30%"],
+    columns: ["auto", "30%"]
   },
   {
     path: "/productivity-solutions-grant/solutionrepo/*",
-    columns: ["10%", "auto", "20%", "10%", "10%"],
+    columns: ["10%", "auto", "20%", "10%", "10%"]
   },
   {
     path: "/energy-efficiency-grant/solutionrepo/*",
-    columns: ["10%", "auto", "10%"],
+    columns: ["10%", "auto", "10%"]
   },
   {
     path: "/enterprisejobskills/programmes-and-initiatives/develop-human-capital/hr-tech-solutions-powered-by-skills-framework/",
-    columns: ["auto", "10%", "10%", "10%", "10%","10%"],
+    columns: ["auto", "10%", "10%", "10%", "10%","10%"]
   }
 ];
 
