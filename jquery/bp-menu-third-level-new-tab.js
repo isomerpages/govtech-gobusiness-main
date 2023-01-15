ce8dd9526b59ca4706642726aae93cd06cc2c253
Yaoxian <yaoxian.gtwork@gmail.com>
2023-01-15 23:12:31 +0800

    Try tweak new tab jquery

--- jquery/bp-menu-third-level-new-tab.js
+++ jquery/bp-menu-third-level-new-tab.js
@@ -2,17 +2,10 @@
 // Note: Add the second <script> file into the respective .md file. If it doesn't work, add the first one too in this order.
 
 const listOfLinks = [
-  "Start Business",
-  "Business Grants Portal",
-  "Covid-19",
-  "Licences and Permits",
-  "For event organisers",
-  "For new food service business owners",
-  "Dashboard",
-  "SkillsFuture for Enterprise",
+  "SFEC Microsite"
 ];
 
-const main = ".bp-menu-list>li>a";
+const main = ".bp-menu-list>third-level-nav-div>li>a";
 const sub = ".third-level-nav-item";
 
 const makeNewTab = sel =>
