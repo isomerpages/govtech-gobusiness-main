fceefeaf461024264f9c9fdf93e9d9c3c1936be8
Yaoxian <yaoxian.gtwork@gmail.com>
2023-03-28 18:18:35 +0800

    Update notifications.js

--- jquery/notifications.js
+++ jquery/notifications.js
@@ -37,6 +37,10 @@ const custom_notifications = [
   //[**SPF DOWNTIME**]
   //"<b>[Attention: System Maintenance]</b> Application, amendment & renewing of Singapore Police Force (SPF) licences will be unavailable during the upcoming scheduled maintenance from 16 Nov 2022 8pm to 17 Nov 2022 7am. <b>All other licences in Guided Journey for Food Services will remain available.</b> We apologise for the inconvenience caused.",
 
+  //[**NEA DOWNTIME**]
+  "<b>[Attention: System Maintenance]</b> Application, amendment & renewing of National Environment Agency (NEA) licences will be unavailable during the upcoming scheduled maintenance from 29 Mar 2023 6:30pm to 11:59pm. We apologise for the inconvenience caused.",
+
+
   //[**SINGPASS**]
   //"<b>[Attention: System Maintenance]</b> Singpass will be unavailable for GoBusiness due to scheduled maintenance on 4 Dec 2022 from 12am to 6am. We apologise for the inconvenience caused.",
   //"<b>[Attention: System Maintenance]</b> GoBusiness Dashboard FAQ and My Task page will be unavailable due to scheduled maintenance from 31 Aug 2022 5pm to 6pm. We apologise for the inconvenience caused."
@@ -49,8 +53,6 @@ const custom_notifications = [
 
   //[**GCC Migration**]
   //"<b>[Attention: System Maintenance]</b> Guided Journey and e-Advisers will be unavailable due to scheduled maintenance on 6 Nov 2022 from 12am to 8am. All other services on GoBusiness will remain available.  We apologise for the inconvenience caused.",
-
-
 
   //[PWMark]
   //"<b>[Attention: System Maintenance]</b> Progressive Wage (PW) Mark will be unavailable due to scheduled maintenance on 12 Mar 2023 from 12am to 8am. We apologise for the inconvenience caused."
