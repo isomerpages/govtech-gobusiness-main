708bd240528d7f48de4cccd70efdae62334a14cf
Yaoxian <yaoxian.gtwork@gmail.com>
2023-04-03 14:46:20 +0800

    Revert "Update maintenance notice page"
    This reverts commit 3645443cd2c35bd92b801eebaffbe909b51aff80.

--- jquery/resize-tables.js
+++ jquery/resize-tables.js
@@ -44,10 +44,6 @@ const customTables = [
     columns: ["50%", "auto"]
   },
   {
-    path: "/maintenance-notices/",
-    columns: ["50%", "auto"]
-  },
-  {
     path: "/enterprisejobskills/programmes-and-initiatives/recruit-talent/sgunited-jobs-and-skills-placement-partners-initiative/",
     columns: ["30%", "auto"]
   },
