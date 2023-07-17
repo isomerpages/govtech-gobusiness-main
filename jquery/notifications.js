7adf71f1fb323e28be9114a065a743c0964816c2
Yaoxian <84306725+chanyaoxian@users.noreply.github.com>
2023-07-17 10:25:19 +0800

    Update notifications.js

--- jquery/notifications.js
+++ jquery/notifications.js
@@ -83,7 +83,7 @@ const custom_notifications = [
 
   //"<b>[Attention: System Maintenance]</b> Application, amendment, renewal & cancellation of National Environment Agency (NEA) licences will be unavailable during the upcoming scheduled maintenance on 12 Jul 2023 from 6.30pm to 11.59pm. We apologise for the inconvenience caused.",
   //"<b>[Attention: System Maintenance]</b> GoBusiness Licensing will be unavailable due to scheduled maintenance from 8pm to 11pm on 12 July 2023. Guided Journey for Food Services will remain available. We apologise for the inconvenience caused.",
-  "<b>[Attention: System Maintenance]</b> GoBusiness Licensing will be unavailable due to maintenance from 15 Jul 2023 2pm to 17 Jul 2023 12am. Guided Journey for Food Services will remain available. We apologise for the inconvenience caused."
+  //"<b>[Attention: System Maintenance]</b> GoBusiness Licensing will be unavailable due to maintenance from 15 Jul 2023 2pm to 17 Jul 2023 12am. Guided Journey for Food Services will remain available. We apologise for the inconvenience caused."
 
 ];
 
