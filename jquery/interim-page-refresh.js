665df6d8ba09a34064edaf455a6951c026d6d5d6
Christopher Lye <clye@palo-it.com>
2024-04-19 15:19:16 +0800

    feat: add interim page refresh redirect

---
+++ jquery/interim-page-refresh.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+	// Retrieve countdown seconds
+	const secondsElement = document.getElementById("seconds");
+	let seconds = secondsElement.textContent;
+
+	// Set destination page
+	const refreshElement = document.getElementById("refresh");
+	const searchParams = new URLSearchParams(document.location.search);
+	const destinationPage = searchParams.get("page");
+	refreshElement.content = `${seconds}; url=${destinationPage}`;
+
+	// Trigger countdown
+	setInterval(() => {
+		seconds -= 1;
+		secondsElement.textContent = seconds;
+	}, 1000);
+});
