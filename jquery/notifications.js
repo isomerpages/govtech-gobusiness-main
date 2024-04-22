4d2983b258f1dcd9843e89482ca3fdc45ee47923
azlynbr <139197306+azlynbr@users.noreply.github.com>
2024-04-22 14:06:08 +0800

    Update notifications.js

--- jquery/notifications.js
+++ jquery/notifications.js
@@ -7,7 +7,7 @@ const custom_notifications = [
   //multiple services
     "<b>[Attention: System Maintenance]</b> GoBusiness Portal is temporarily refining our search capabilities for a better experience. Please bear with us as you may encounter some limitations in searching for records during this time. We apologise for the inconvenience caused.",
 "<b>[Attention: System Maintenance]</b> Multiple services will be unavailable while they are being upgraded. Please view the <B><u><a href='https://go.gov.sg/gobiz-maintenance-notice' target='_blank' rel='noopener'>upcoming maintenance schedule</a></u></B> for details of affected services. We apologise for the inconvenience caused."
-  
+"<b>[Attention: System Maintenance for NEA]</b> All NEA Licences & Submission of Returns on GoBusiness Portal will be unavailable from (i) 3 May 3PM to 6 May 10 AM and (ii) 23 May 3PM to 27 May 10AM for maintenance. We apologise for the inconvenience caused."  
 ];
 
 
